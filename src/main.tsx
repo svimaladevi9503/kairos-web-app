05cfb31acb27ff228544c0af3eac3095889f660a
vimaladevi950 <svimaladevi9503@gmail.com>
2026-06-29 04:14:47 +0530

    feat: initialize React root and Firebase service in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { FirebaseService } from './database';
+import { FirebaseService } from './database/firebase';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
